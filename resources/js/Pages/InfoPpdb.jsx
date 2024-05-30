5f79f3fcc3c208d995dfa9ba290c441fb009947e
Mohammad Mirza Qusyairi <mirza4909@gmail.com>
2024-05-30 12:03:46 +0700

    [update] info ppdb

--- resources/js/Pages/InfoPpdb.jsx
+++ resources/js/Pages/InfoPpdb.jsx
@@ -2,31 +2,59 @@ import ContentTitle from '@/Components/ContentTitle';
 import DefaultLayout from '@/Layouts/DefaultLayout';
 import React from 'react';
 import '../../css/ppdb.css';
+import SubTitle from '@/Components/SubTitle';
 
 function InfoPpdb() {
     const imageData = [
-        'images/ppdb/image1.jpeg',
-        'images/ppdb/image2.jpeg',
-        'images/ppdb/image3.jpeg',
-        'images/ppdb/image4.jpeg',
-        'images/ppdb/image5.jpeg',
-        'images/ppdb/image6.jpeg',
-        'images/ppdb/image7.jpg',
-        'images/ppdb/image8.jpg',
+        {
+            "title": "JADWAL PPDB 2024 TAHAP 2",
+            "image": "images/ppdb/image1.jpeg",
+        },
+        {
+            "title": "INFORMASI PPDB",
+            "image": "images/ppdb/image2.jpeg",
+        },
+        {
+            "title": "SOSIAL MEDIA SMKN 10 BANDUNG",
+            "image": "images/ppdb/image3.jpeg",
+        },
+        {
+            "title": "INFORMASI DAFTAR ULANG",
+            "image": "images/ppdb/image4.jpeg",
+        },
+        {
+            "title": "CONTACT PERSON PPDB SMKN 10 BANDUNG",
+            "image": "images/ppdb/image5.jpeg",
+        },
+        {
+            "title": "JADWAL TES WAWANCARA",
+            "image": "images/ppdb/image6.jpeg",
+        },
+        {
+            "title": "DOKUMEN PERSYARATAN UMUM",
+            "image": "images/ppdb/image7.jpg",
+        },
+        {
+            "title": "DOKUMEN PERSYARATAN KHUSUS",
+            "image": "images/ppdb/image8.jpg",
+        },
     ];
 
     return (
         <DefaultLayout>
             <ContentTitle subTitle='INFO PPDB'/>
-            <div className="flex flex-wrap justify-center">
-                {imageData.map((url, index) => (
-                    <img
-                        key={index}
-                        src={url}
-                        alt={`PPDB Image ${index + 1}`}
-                        className='content m-2 shadow-md lg:h-[600px] sm:h-[184px] slide-in-up'
-                        style={{objectFit: 'cover'}}
-                    />
+            <div className="flex flex-col items-center">
+                {imageData.map((data, index) => (
+                    <div>
+                        <div className={`mb-6 font-semibold sm:text-[25px] md:text-[30px]`}>{data.title}</div>
+                        <img
+                            key={index}
+                            src={data.image}
+                            alt={`PPDB Image ${index + 1}`}
+                            className='content m-2 shadow-md lg:h-[600px] sm:h-[184px] slide-in-up mb-20'
+                            style={{objectFit: 'cover'}}
+                        />
+                    </div>
                 ))}
             </div>
         </DefaultLayout>
