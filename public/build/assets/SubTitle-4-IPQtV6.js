4e5a062a7783b73e11d25f5598f4a6052416a635
BukanRaychan <fasyaraihan82@gmail.com>
2024-07-14 14:29:44 +0700

    deploy build

--- public/build/assets/SubTitle-4-IPQtV6.js
+++
@@ -1,2 +0,0 @@
-import{j as e}from"./app-B7e2hPpL.js";function m({title:l,containerClassName:t=null}){return e.jsx("div",{className:`${t??"mb-6 mt-9"}  
-        font-semibold text-[32px] `,children:l})}export{m as S};
