7a68a90bbde604ffc294bd0c7dabf6c02f5bf93a
BukanRaychan <fasyaraihan82@gmail.com>
2024-07-29 21:26:49 +0700

    deploy build

--- public/build/assets/TextAreaInput-Cqoo-ZDN.js
+++
@@ -1,1 +0,0 @@
-import{r,j as f}from"./app-CuIkUGmS.js";const d=r.forwardRef(function({type:s="text",className:o="",children:n,isFocused:a=!1,...u},e){const t=e||r.useRef();return r.useEffect(()=>{a&&t.current.focus()},[]),f.jsx("textarea",{...u,type:s,className:"border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm "+o,ref:t,children:n})});export{d as T};
