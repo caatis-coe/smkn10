3cb8c6d0d068a91bdefea328a8236996a2a0e49a
BukanRaychan <fasyaraihan82@gmail.com>
2024-08-04 23:06:37 +0700

    deploy build

--- public/build/assets/SubTitle-BC2z0img.js
+++
@@ -1,2 +0,0 @@
-import{j as e}from"./app-CZfLhPAE.js";function m({title:l,containerClassName:t=null}){return e.jsx("div",{className:`${t??"mb-6 mt-9"}  
-        font-semibold text-[32px] `,children:l})}export{m as S};
